e2fa09ebd54388a639cdb351ce3d9070f8ef29a8
corrimori <corrine@promptt.com>
2018-10-14 01:06:50 -0700

    update production to deploy

--- knexfile.js
+++ knexfile.js
@@ -15,8 +15,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection:
-      'fillInLater',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './db/migrations'
     },
